7007494e1ddc9aca70d5b64b2db5adf5240a1b09
Cláudio Luiz <barthy@gmail.com>
2015-08-14 15:44:29 -0300

    Tentando fazer o Scaffolding trabalhar junto com a listagem manual (estatica) do BD

--- grails-app/controllers/tutorialclaudio2/UsuarioController.groovy
+++ grails-app/controllers/tutorialclaudio2/UsuarioController.groovy
@@ -1,15 +1,16 @@
 package tutorialclaudio2
 
 class UsuarioController {
-	//def scaffold = Usuario
-	def index() {
-		/*
-		Usuario usuario = new Usuario(nome: 'John')
+	def scaffold = Usuario
+	/*
+	def index() {				
+		//Aqui não vai usar o BD, estou instanciando a classe na mao
+		//Usuario usuario = new Usuario(nome: 'John')
 		//a linha de baixo diz que na view havera um objeto chamado "usuario" proviniente dessa instaciacao feita acima
-		[ usuario:usuario ]
-		*/
+		//[ usuario:usuario ]
 		
-		def usuarios = Usuario.list()
-		[usuarios:usuarios]
-	}
+		//Disponibilizando uma lista, aqui sim eu vou usar o banco de dados. 
+		//def usuarios = Usuario.list()
+		//[usuarios:usuarios]		
+	}*/
 }
